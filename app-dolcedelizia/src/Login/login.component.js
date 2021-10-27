1a7c0da680be51242c97cbbc2ed25fe61ab79d83
orviant20011904 <92343935+orviant20011904@users.noreply.github.com>
2021-10-27 12:42:00 -0500

    Modificacion Login
    Validacion Campos obligatorios

--- app-dolcedelizia/src/Login/login.component.js
+++ app-dolcedelizia/src/Login/login.component.js
@@ -1,29 +1,80 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Form,Group,Label,Text,Button,Control,Alert,Card} from "react-bootstrap";
 import {useForm} from 'react-hook-form';
-import './login.css';   
+import './login.css';  
+
+    
 
     
     const Login = () => {
+        const {register,handleSubmit, formState: { errors }} = useForm();
+        const onsubmit = (data) =>{
+            console.log(data);
+        }
 
     return (
 
         <div className="Login">
-        <Form style={{widthmax: '1500px', height: '100%'}}className="login-form">
+        <Form onSubmit={handleSubmit(onsubmit)}
+        style={{ widthmax: '1500px', 
+                 height: '100%'}}
+                 
+                 className="login-form">
             
                 <h1 style={{color: 'black'}}>INICIO DE SESION</h1>
                 <br/>
            
 
             <Form.Group >
-                <Form.Label style={{color: 'black', fontSize:'15px'}}><b>USUARIO <br/></b> </Form.Label>
+                <Form.Label 
+                    style={{color: 'black', fontSize:'15px'}}>
+                        <b>USUARIO <br/></b> 
+                </Form.Label>
                 <br/>
-                <Form.Control style={{color: 'black',paddingtop: '10px' }} type="email" placeholder="Enter User" />
+                <Form.Control 
+                    name="usuario"
+                    style={{color: 'black',paddingtop: '10px' }} 
+                    type="text" 
+                    placeholder="Enter User" 
+
+                    {...register("usuario", { 
+                        required:{
+                            value: true,
+                            message: 'Campo Obligatorio' 
+                        }
+                    })}   
+                    />
+                    
+                    <span 
+                    style={{fontSize:'15px'}}
+                    className="text-danger text-small d-block ">
+                        {errors.usuario &&  errors.usuario.message}
+                    </span>
                 
-                <Form.Group className="mb-6" controlId="formBasicPassword">
-                    <Form.Label required style={{color: 'black', fontSize:'15px'}}><b><br/>CONTRASEÑA<br/> </b> </Form.Label>
+                <Form.Group className="mt-6" controlId="formBasicPassword">
+                    <Form.Label required
+                        style={{color: 'black', fontSize:'15px'}}>
+                        <b><br/>CONTRASEÑA<br/> </b> 
+                    
+                    </Form.Label>
                     <br/>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control
+                        name="contraseña"
+                        {...register("contraseña", { 
+                            required:{
+                                value: true,
+                                message: 'Campo Obligatorio' 
+                            }
+                        })}
+                        type="password" 
+                        placeholder="Password" />
+                        
+                        
+                    <span 
+                     style={{fontSize:'15px'}}
+                    className="text-danger text-small d-block ">
+                        {errors.contraseña &&  errors.contraseña.message}
+                    </span>
                 </Form.Group>
             
                 
