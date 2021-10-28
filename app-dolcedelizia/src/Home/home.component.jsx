881877280e9330af836e410149c446ba689be925
orviant20011904 <92343935+orviant20011904@users.noreply.github.com>
2021-10-28 10:41:43 -0500

    Update home.component.jsx

--- app-dolcedelizia/src/Home/home.component.jsx
+++ app-dolcedelizia/src/Home/home.component.jsx
@@ -5,11 +5,11 @@ import Lista from './list.product'
 
 const HomeDolceDelizia = () => {
     const [items,setItems]= useState([
-    {id:'1',nombre: 'postre de fresa',precio: 600,img: 'https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2018/01/paydefresagelatina.png'},
-    {id:'2',nombre: 'postre de chocolate',precio: 800,img: 'https://www.hogarmania.com/archivos/201903/postres-chocolate-668x400x80xX.jpg'},
-    {id:'3',nombre: 'postre de maracuya',precio:500,img: 'https://images.aws.nestle.recipes/resized/7ca2437097d401c51d4a759f4ad56b11_maracuya_1200_600.jpeg'},
-    {id:'4',nombre: 'malteada de chocolate',precio:250,img:'https://losmilagrosdedalila.com/wp-content/uploads/2017/04/MALTEADA-FRESA-9658.jpg'},
-    {id:'5',nombre: 'malteada de chocolate',precio: 180, img:'https://www.vitamix.com/media/recipe/rcpChocolateShake/images/chocolatemilkshakemainjpg.jpg'}
+    {id:'1',nombre: 'Postre de Fresa',precio: 600,img: 'https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2018/01/paydefresagelatina.png'},
+    {id:'2',nombre: 'Postre de Chocolate',precio: 800,img: 'https://www.hogarmania.com/archivos/201903/postres-chocolate-668x400x80xX.jpg'},
+    {id:'3',nombre: 'Postre de Maracuya',precio:500,img: 'https://images.aws.nestle.recipes/resized/7ca2437097d401c51d4a759f4ad56b11_maracuya_1200_600.jpeg'},
+    {id:'4',nombre: 'Malteada de Fresa',precio:250,img:'https://losmilagrosdedalila.com/wp-content/uploads/2017/04/MALTEADA-FRESA-9658.jpg'},
+    {id:'5',nombre: 'Malteada de Chocolate',precio: 180, img:'https://www.vitamix.com/media/recipe/rcpChocolateShake/images/chocolatemilkshakemainjpg.jpg'}
     ]);
     return (
     <Fragment>
