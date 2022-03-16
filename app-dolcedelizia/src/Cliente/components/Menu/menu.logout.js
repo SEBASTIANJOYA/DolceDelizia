25c0b418770a7e18abbcdb9b966ed28efcb8b2fb
orviant20011904 <92343935+orviant20011904@users.noreply.github.com>
2022-03-16 14:36:18 -0500

    Modificacion Front del menu logout

--- app-dolcedelizia/src/Cliente/components/Menu/menu.logout.js
+++ app-dolcedelizia/src/Cliente/components/Menu/menu.logout.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 import {Navbar,Button,Container,Nav,Form,FormControl, Row,Col} from "react-bootstrap";
 import logo from './dolcedelizia.png';
+import logocarrito from './carrito.png'
+
+
 
 const Home = () => {
+  
     return (
-        <Navbar  collapseOnSelect="true" role="navigation" bg="blue" expand="md" style={
+        <Navbar className="navbar navbar-dark bg-primary " collapseOnSelect="true" role="navigation" bg="blue" expand="md" style={
           {
-            fontSize:'16px'
+            
+            fontSize:'16px',
+            
           }
         }>
         <Container fluid>
@@ -15,40 +21,55 @@ const Home = () => {
           <img src={logo} alt="logo" style={{width:'50px'}}/>
             <b >     DOLCE DELIZIA</b></Navbar.Brand>
           <Navbar.Toggle aria-controls=" basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" style={{withmax:'100%'}}>
-            <Row style={{margin:'auto'}}>
+          <Navbar.Collapse id="basic-navbar-nav" style={{widthmax:'100%',}}>
+            <Row style={{margin:'auto', marginLeft:'50px'}}>
             <Nav
               className="mr-auto me-auto "
-              style={{ maxHeight: '500px',  }}
+              style={{ maxHeight: '250px',  }}
               navbarScroll
             >
-              <Col lg={2}>
+              <Col lg={4}>
               <Nav.Link href="/homedolcedelizia"><b>INICIO</b></Nav.Link>
               </Col>
-              <Col lg={6}>
-              <Nav.Link href=""><b>QUIENES SOMOS ?</b></Nav.Link>
+              <Col lg={4}>
+              <Nav.Link href=""><b>QUIENES SOMOS</b></Nav.Link>
               </Col>
-              <Col lg={3}>
+              <Col lg={4}>
               <Nav.Link href=""><b>CONTACTOS</b></Nav.Link>
               </Col>
-              <Col lg={3}>
+              <Col lg={4}>
               <Nav.Link href=""><b>AYUDA</b></Nav.Link>
               </Col>
               
-              <Col lg={2}>
+              <Col lg={3}>
 
               <Nav.Link href="/login"><b>LOGIN</b></Nav.Link>
               </Col>
+
+              
+
              
             </Nav>
-            </Row>
-            <nav>
             
-            <Form className="d-flex" style={{margin:'default'}} >
-              <Button href="/login" variant="outline-info form-control"><b>Login</b></Button>
 
-            </Form>
-            </nav>
+
+            </Row>
+            <Nav>
+              
+
+                              
+                <a href="/carrito" id="carrito_logout"><img src={logocarrito}  alt="logo" style={{width:'50px'}}/><span className="badge  ml-2"
+                style={{background:'red'}}>{
+
+                  (localStorage.getItem('items')==null)?
+                  0:
+                  JSON.parse(localStorage.getItem('items')).length
+                }</span></a> 
+
+
+              
+            
+            </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
